20a8edce78d86adc45c2843d0006980d6ba6c529
Janne Rönkkö <janne.ronkko@iki.fi>
2022-01-14 18:41:35 +0200

    Use setup.cfg for defining everything else except the version

--- setup.py
+++ setup.py
@@ -16,36 +16,6 @@ except ImportError:
     VERSION = '0.0+tox'
 
 
-with open('README.rst', 'rt', encoding='utf-8') as readme_file:
-    long_desc = readme_file.read()  # pylint: disable=invalid-name
-
 setuptools.setup(
-    name='vcsver',
     version=VERSION,
-    description='Automatic package version numbering from version control',
-    long_description=long_desc,
-    long_description_content_type='text/x-rst',
-    url='https://github.com/janneronkko/vcsver',
-    author='Janne Rönkkö',
-    author_email='janne.ronkko@iki.fi',
-    license='MIT',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: System :: Archiving :: Packaging',
-    ],
-    keywords='setuptools development git version',
-    packages=['vcsver'],
-    python_requires='>=3.7',
-    entry_points={
-        'distutils.setup_keywords': [
-            'vcsver = vcsver.setuptools:vcsver',
-        ],
-    },
 )
