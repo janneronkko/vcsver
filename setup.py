6af7d7ccdc4583cec4e921e5e8214c719fb517db
Janne Rönkkö <janne.ronkko@iki.fi>
2022-01-14 18:41:36 +0200

    Remove build time dependency to source tree
    This fixes python -m build

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
-import os
-
 import setuptools
-
-try:
-    import vcsver
-    VERSION = vcsver.get_version()
-
-except ImportError:
-    if 'TOX_WORK_DIR' not in os.environ:
-        # When running inside TOX, vcsver is not installed in
-        # the virtual environment created by TOX because we can not depend
-        # on ourself when installing
-        raise
-
-    VERSION = '0.0+tox'
 
 
 setuptools.setup(
-    version=VERSION,
+    vcsver=True,
 )
